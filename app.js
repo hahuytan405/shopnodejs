0e8a2b2800ff7620d5717c3fdf051d694f55a4b8
Tan <hahuytan405@gmail.com>
2023-05-30 16:35:06 +0700

    first commit

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ const compression = require('compression');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI =
-  'mongodb+srv://max:Tannhim12@cluster0.9tnnpex.mongodb.net/shop?retryWrites=true&w=majority';
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.9tnnpex.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
 
 const app = express();
 const store = new MongoDBStore({
@@ -123,7 +122,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
